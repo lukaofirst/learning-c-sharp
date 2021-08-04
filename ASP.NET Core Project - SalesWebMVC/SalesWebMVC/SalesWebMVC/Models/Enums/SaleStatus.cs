790a4eb760d8e2c433c9d20817865aabc529c06e
Lucas Macedo <lucasmacedo123@gmail.com>
2021-08-04 16:53:08 -0300

    adding ASP.NET Core Project

---
+++ ASP.NET Core Project - SalesWebMVC/SalesWebMVC/SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesWebMVC.Models.Enums {
+    public enum SaleStatus {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
---
+++ ASP.NET Core Project - SalesWebMVC/SalesWebMVC/SalesWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using SalesWebMVC.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMVC.Services {
+    public class DepartmentService {
+        private readonly SalesWebMVCContext _context;
+
+        public DepartmentService(SalesWebMVCContext context) {
+            _context = context;
+        }
+
+        public async Task<List<Department>> FindAllAsync() {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
+        }
+    }
+}
