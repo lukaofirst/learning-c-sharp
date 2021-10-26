000dd0595fc7a15cffea665035945b368f741a25
lukaofirst <lucasmacedo123@gmail.com>
2021-10-26 01:29:52 -0300

    Creating project, models and data context

---
+++ EF Core - Web API/EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,18 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data {
+    public class HeroiContext : DbContext {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+}
---
+++ EF Core - Web API/EFCore.WebAPI/Models/Heroi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Models {
+    public class Heroi {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Batalha Batalha { get; set; }
+        public int BatalhaId { get; set; }
+    }
+}
