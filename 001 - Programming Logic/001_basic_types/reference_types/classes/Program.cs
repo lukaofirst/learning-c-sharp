c66d88caff99ae40cac38b18008e3592f03b2a5f
lukaofirst <lucasmacedo123@gmail.com>
2023-01-31 18:58:00 -0300

    Basic Types - classes

---
+++ 001 - Programming Logic/001_basic_types/reference_types/classes/Program.cs
@@ -0,0 +1,11 @@
+﻿using classes;
+
+var person1 = new Person("Lorem", 25);
+
+var person2 = person1;
+
+person2.Name = "Ipsum";
+person2.Age = 22;
+
+Console.WriteLine(person1.Name);
+Console.WriteLine(person1.Age);
