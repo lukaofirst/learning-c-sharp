edbdc55f9284900edeaecd81ea19a8f35b09181d
lukaofirst <lucasmacedo123@gmail.com>
2023-01-31 18:30:22 -0300

    Basic Types - nullables

---
+++ 001 - Programming Logic/001_basic_types/value_types/nullables/Program.cs
@@ -0,0 +1,18 @@
+﻿/* --- Basic Types --- */
+
+/* - nullables - */
+Nullable<int> num1 = null;
+
+num1 = 10;
+
+if(num1.HasValue)
+    Console.WriteLine(num1);
+
+// ? operator - shorthand syntax (recommended usage)
+int? num2 = null;
+Console.WriteLine(num2.GetValueOrDefault());
+
+// ?? operator - Nullish Coalescing
+// if num2 is null, return the right side, in this case 50;
+var result = num2 ?? 50;
+Console.WriteLine(result);
