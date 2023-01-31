01aa700d5e80b106c7a3f761fc05dffc1d8e86ef
lukaofirst <lucasmacedo123@gmail.com>
2023-01-31 18:30:31 -0300

    Basic Types - strings

---
+++ 001 - Programming Logic/001_basic_types/reference_types/strings/Program.cs
@@ -0,0 +1,27 @@
+﻿/* --- Basic Types --- */
+
+/* - strings - */
+var example = "Lorem";
+
+// string with special characters 
+var specialStr = "I'm a special string \n another line";
+
+// Verbatim strings
+var verbatimStr = @"I'm a ""verbatim"" string 
+    with break line";
+
+Console.WriteLine(example);
+Console.WriteLine(specialStr);
+Console.WriteLine(verbatimStr);
+Console.WriteLine();
+
+// Concatenation
+var firstName = "Lorem";
+var lastName = "Ipsum";
+Console.WriteLine("Concatenation => The fullname is:" + " " + firstName + " " + lastName);
+
+// Composite Formatting
+Console.WriteLine("Composite Formatting => The fullname is {0} {1}", firstName, lastName);
+
+// Interpolation (the recommended approach)
+Console.WriteLine($"Interpolation => The fullname is {firstName} {lastName}");
