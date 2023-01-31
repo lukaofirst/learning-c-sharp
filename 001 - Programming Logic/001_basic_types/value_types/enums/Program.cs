f58e5120a56edb74f3b98ec51a68e33482111e29
lukaofirst <lucasmacedo123@gmail.com>
2023-01-31 18:29:41 -0300

    Basic Types - enums

---
+++ 001 - Programming Logic/001_basic_types/value_types/enums/Program.cs
@@ -0,0 +1,18 @@
+﻿using enums;
+
+/* --- Basic Types --- */
+
+/* - enums - */
+
+var sunday = WeekDayEnum.Sunday;
+var parsedIntToEnum = (WeekDayEnum) 2;
+var parsedStringToEnum = Enum.Parse<WeekDayEnum>("3");
+
+Console.WriteLine(sunday);
+Console.WriteLine((int) sunday);
+
+Console.WriteLine(parsedIntToEnum);
+Console.WriteLine((int) parsedIntToEnum);
+
+Console.WriteLine(parsedStringToEnum);
+Console.WriteLine((int) parsedStringToEnum);
---
+++ 001 - Programming Logic/001_basic_types/value_types/enums/WeekDayEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace enums
+{
+    public enum WeekDayEnum
+    {
+        Sunday, // by default, it starts with 0 and incremented by 1
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
