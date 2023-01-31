c8771d46ff9e199abf874f664d9052cf7749775a
lukaofirst <lucasmacedo123@gmail.com>
2023-01-31 18:29:54 -0300

    Basic Types - structs

---
+++ 001 - Programming Logic/001_basic_types/value_types/structs/PersonStruct.cs
@@ -0,0 +1,17 @@
+﻿namespace structs
+{
+    public struct PersonStruct
+    {
+        public string Name { get; set; }
+
+        public PersonStruct(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ 001 - Programming Logic/001_basic_types/value_types/structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using structs;
+
+/* --- Basic Types --- */
+
+/* - structs - */
+PersonStruct person1;
+person1 = new("Lorem");
+
+PersonStruct person2 = person1;
+person2.Name = "Ipsum";
+
+// Remember, struct is a value type
+// so, the result here is going to be "Lorem"
+Console.WriteLine(person1.ToString());
+
+// if PersonStruct is declared as a class,
+// the output, in this case is going to be "Ipsum"
+
+
