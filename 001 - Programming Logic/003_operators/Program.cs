0be9c8369643f12ce6b4cd821291406ac2abf33f
lukaofirst <lucasmacedo123@gmail.com>
2023-02-01 10:41:38 -0300

    Operators (Comparatives and Logicals)

---
+++ 001 - Programming Logic/003_operators/Program.cs
@@ -0,0 +1,17 @@
+﻿/* --- Operators --- */
+
+// Comparatives
+// > - Greater than
+// >= Greater or Equal than
+// < - Less than
+// <= Less or Equal than
+// == - Equal
+// != - Not Equal
+
+// Logical
+// && - AND
+// || - OR
+// ! - NOT
+
+// Who comes first (logical operators)?
+// ! > && > ||
