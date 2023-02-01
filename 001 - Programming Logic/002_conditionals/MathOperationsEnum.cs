9da714e4cecd32e29b99aa374634a0594829cc2d
lukaofirst <lucasmacedo123@gmail.com>
2023-02-01 10:40:53 -0300

    Conditionals

---
+++ 001 - Programming Logic/002_conditionals/MathOperationsEnum.cs
@@ -0,0 +1,10 @@
+namespace _002_conditionals
+{
+	public enum MathOperationsEnum
+	{
+		Add = 1,
+		Subtract,
+		Multiply,
+		Divide
+	}
+}
---
+++ 001 - Programming Logic/002_conditionals/Program.cs
@@ -0,0 +1,39 @@
+﻿using _002_conditionals;
+
+var age = 25;
+const int adultAge = 21;
+
+// Using if-else
+if (age >= adultAge)
+{
+	Console.WriteLine("He/she is adult");
+}
+else
+{
+	Console.WriteLine("He/she is not adult");
+}
+
+// Using ternary operator
+var myAge = 23;
+var amIAdult = myAge >= adultAge ? true : false;
+
+// Using switch-case
+var pickOperation = (MathOperationsEnum)3;
+
+switch (pickOperation)
+{
+	case MathOperationsEnum.Add:
+		Console.WriteLine("Sum");
+		break;
+	case MathOperationsEnum.Subtract:
+		Console.WriteLine("Subtract");
+		break;
+	case MathOperationsEnum.Multiply:
+		Console.WriteLine("Multiply");
+		break;
+	case MathOperationsEnum.Divide:
+		Console.WriteLine("Divide");
+		break;
+	default:
+		throw new Exception("Something went wrong");
+}
