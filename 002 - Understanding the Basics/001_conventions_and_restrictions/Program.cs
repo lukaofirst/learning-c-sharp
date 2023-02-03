e12e61aa87e23ce0698e41376175a200711bfe67
lukaofirst <lucasmacedo123@gmail.com>
2023-02-02 21:24:38 -0300

    Understanding the Basics - Conventions and Restrictions

---
+++ 002 - Understanding the Basics/001_conventions_and_restrictions/Program.cs
@@ -0,0 +1,21 @@
+﻿/* --- Conventions and Restrictions --- */
+
+/* - Conventions - */
+
+// camelCase
+// * method parameters
+// * variables inside a method
+
+// PascalCase
+// * namespaces
+// * classes
+// * properties
+// * methods
+
+// the "underscore" (_lastName)
+// * Class's private fields
+
+/* - Restrictions - */
+// * It cannot start with a numeric digit, use a letter or underscore
+// * It cannot use accents
+// * It cannot use whitespaces
