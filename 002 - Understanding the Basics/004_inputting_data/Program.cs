dc5e68d3b0d16fb7ef7e1bea9dfee9a79bd53dfb
lukaofirst <lucasmacedo123@gmail.com>
2023-02-02 21:25:59 -0300

    Understanding the Basics - Inputting Data

---
+++ 002 - Understanding the Basics/004_inputting_data/Program.cs
@@ -0,0 +1,24 @@
+﻿/* --- Inputting Data --- */
+Console.WriteLine("Type something on console");
+var input = Console.ReadLine();
+Console.WriteLine(input);
+Console.WriteLine();
+
+// breaking input into multiple items, separated by ", "
+Console.WriteLine("Breaking input into multiple items, separed by \", \"");
+var inputs = Console.ReadLine();
+
+var splittedInputs = inputs!.Split(", ");
+
+foreach (var item in splittedInputs)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+
+// converting input data (for example to integers)
+Console.WriteLine("Type a number to convert to an integer");
+var integerInput = Console.ReadLine();
+
+var result = int.Parse(integerInput!);
+Console.WriteLine(result);
