7995ac1b326880118682eb85cf5f3ba54bc16787
lukaofirst <lucasmacedo123@gmail.com>
2023-02-02 21:25:45 -0300

    Understanding the Basics - Type Casting

---
+++ 002 - Understanding the Basics/003_type_casting/Program.cs
@@ -0,0 +1,20 @@
+﻿/* --- Type Casting --- */
+
+// Implicit Casting
+int num1 = 10;
+double num2 = num1;
+Console.WriteLine(num2);
+
+// Explicit Casting
+double num3 = 20.2345;
+int num4 = (int)num3;
+Console.WriteLine(num4);
+
+// Type Conversion Methods
+float num5 = 34903.2390F;
+var num6 = Convert.ToInt32(num5);
+Console.WriteLine(num6);
+
+string isAdult = "true";
+var num7 = Convert.ToBoolean(isAdult);
+Console.WriteLine(num7);
