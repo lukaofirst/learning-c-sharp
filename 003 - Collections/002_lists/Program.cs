cbd0ba5840f4da839aab43b85550518614226efd
lukaofirst <lucasmacedo123@gmail.com>
2023-02-06 07:17:25 -0300

    Collections - lists

---
+++ 003 - Collections/002_lists/Program.cs
@@ -0,0 +1,16 @@
+﻿/* --- Lists --- */
+
+var list = new List<string>();
+var arr = new string[] { "hello", "world" };
+
+list.Add("lorem");
+list.Add("ipsum");
+list.Add("dolor");
+
+list.AddRange(arr);
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
