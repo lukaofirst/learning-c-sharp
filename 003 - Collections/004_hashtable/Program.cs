2792eef0715c2fb034e0e8ba05f1c29acfeb1f84
lukaofirst <lucasmacedo123@gmail.com>
2023-02-06 08:31:48 -0300

    Collections - hashtable

---
+++ 003 - Collections/004_hashtable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+/* --- Hashtable --- */
+
+var hashTable = new Hashtable()
+{
+    { 1, "lorem" }
+};
+
+hashTable.Add("2", "two");
+hashTable.Add("hello", "3");
+
+foreach (DictionaryEntry item in hashTable)
+{
+    Console.WriteLine($"hash code: {item.GetHashCode()} | key: {item.Key} | value: {item.Value}");
+}
