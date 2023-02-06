22d47e6bc2b008baf4940e833cbda6d0605048b2
lukaofirst <lucasmacedo123@gmail.com>
2023-02-06 08:21:04 -0300

    Collections - dictionary

---
+++ 003 - Collections/003_dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿/* --- Dictionary --- */
+
+var dict = new Dictionary<int, string>();
+
+dict.Add(1, "lorem");
+dict.Add(2, "ipsum");
+
+foreach (var item in dict)
+{
+    Console.WriteLine($"hash code: {item.GetHashCode()} | key: {item.Key} | value: {item.Value}");
+}
+
+var cities = new Dictionary<string, string>()
+{
+    { "United Kingdom", "Liverpool, London, Manchester" },
+    { "Spain", "Barcelona, Madrid, Sevilla" }
+};
+
+if (cities.ContainsValue("France"))
+    Console.WriteLine("It contains France");
