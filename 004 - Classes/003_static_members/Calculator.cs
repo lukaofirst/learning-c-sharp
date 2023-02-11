37fa4f9c6c07203f69c7fc8ea110044b12fb4759
lukaofirst <lucasmacedo123@gmail.com>
2023-02-10 23:57:58 -0300

    Classes - static members

---
+++ 004 - Classes/003_static_members/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace _003_static_members
+{
+    public static class Calculator
+    {
+        public static string Sum(double a, double b) => (a + b).ToString("F2");
+        public static string Subtract(double a, double b) => (a - b).ToString("F2");
+        public static string Multiple(double a, double b) => (a * b).ToString("F2");
+        public static string Divide(double a, double b)
+        {
+            if (b.Equals(0))
+                throw new DivideByZeroException();
+
+            return (a / b).ToString("F2");
+        }
+    }
+}
---
+++ 004 - Classes/003_static_members/Program.cs
@@ -0,0 +1,14 @@
+﻿using _003_static_members;
+
+/* --- Static Members --- */
+
+// If a method doesn't access a instance member,
+// the IDE will suggest you to mark as static
+
+var sumResult = Calculator.Sum(20.1, 902.2);
+var subtractResult = Calculator.Subtract(5.24, 0.23);
+var poweredNumber = Math.Pow(10, 2);
+
+Console.WriteLine(sumResult);
+Console.WriteLine(subtractResult);
+Console.WriteLine(poweredNumber);
