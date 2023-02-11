b0935c7dd50d4c3b5f8be64ead0038c5bf05d2f2
lukaofirst <lucasmacedo123@gmail.com>
2023-02-10 23:32:41 -0300

    Classes - classes and objects

---
+++ 004 - Classes/001_classes_and_objects/Entities/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace _001_classes_and_objects.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        // a parameterless constructor
+        public Person()
+        {
+
+        }
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ 004 - Classes/001_classes_and_objects/Program.cs
@@ -0,0 +1,17 @@
+﻿using _001_classes_and_objects.Entities;
+
+/* --- Classes and Objects --- */
+
+// Classes are blueprints for their instances
+
+// person1 and person2 is an instance/object of Person's class
+var person1 = new Person(1, "Lorem", 25);
+
+// * Alternative syntax to initialize values
+// use in case if you have a parameterless constructor
+var person2 = new Person()
+{
+    Id = 2,
+    Name = "Ipsum",
+    Age = 22
+};
