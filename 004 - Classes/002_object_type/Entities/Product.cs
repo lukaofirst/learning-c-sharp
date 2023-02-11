15fcca520b8b8df8874af4300ffca20c866e5dc9
lukaofirst <lucasmacedo123@gmail.com>
2023-02-10 23:48:44 -0300

    Classes - object type

---
+++ 004 - Classes/002_object_type/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace _002_object_type.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} | Name: {Name} | Price: {Price}";
+        }
+    }
+}
---
+++ 004 - Classes/002_object_type/Program.cs
@@ -0,0 +1,29 @@
+﻿/* --- Object Type --- */
+
+using _002_object_type.Entities;
+
+object example = "hello world";
+object num1 = 10;
+object isOkay = false;
+object product1 = new Product(1, "TV", 1400);
+object product2 = new Product(2, "Smartphone", 1800);
+
+Console.WriteLine(example);
+Console.WriteLine(example.GetHashCode());
+Console.WriteLine(example.GetType());
+Console.WriteLine();
+
+Console.WriteLine(num1);
+Console.WriteLine(num1.GetHashCode());
+Console.WriteLine(num1.GetType());
+Console.WriteLine();
+
+Console.WriteLine(isOkay.Equals(false));
+Console.WriteLine();
+
+Console.WriteLine(product1.ToString());
+Console.WriteLine(product2.ToString());
+Console.WriteLine();
+
+Console.WriteLine(ReferenceEquals(product1, product1));
+Console.WriteLine(ReferenceEquals(product1, product2));
