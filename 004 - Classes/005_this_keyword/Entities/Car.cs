0eda26fd011edafdd30adcaa262dd0052e6a6d71
lukaofirst <lucasmacedo123@gmail.com>
2023-02-13 07:15:50 -0300

    Classes - this keyword

---
+++ 004 - Classes/005_this_keyword/Entities/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace _005_this_keyword.Entities
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+
+        public Car(Guid Id, string Name, string Model)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Model = Model;
+        }
+    }
+}
---
+++ 004 - Classes/005_this_keyword/Program.cs
@@ -0,0 +1,5 @@
+﻿using _005_this_keyword.Entities;
+
+/* --- this keyword --- */
+
+var car1 = new Car(Guid.NewGuid(), "Mercedes", "A250");
