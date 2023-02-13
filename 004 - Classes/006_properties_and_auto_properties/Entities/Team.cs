8b445006a7ee6b891d8c12ff39b56bd71c1b9124
lukaofirst <lucasmacedo123@gmail.com>
2023-02-13 07:16:03 -0300

    Classes - properties and auto properties

---
+++ 004 - Classes/006_properties_and_auto_properties/Entities/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace _006_properties_and_auto_properties.Entities
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int FoundedYear { get; set; }
+    }
+}
---
+++ 004 - Classes/006_properties_and_auto_properties/Program.cs
@@ -0,0 +1,10 @@
+﻿using _006_properties_and_auto_properties.Entities;
+
+/* --- Properties and Auto Properties --- */
+
+var team1 = new Team()
+{ 
+    Id = 1, 
+    Name = "Lorem", 
+    FoundedYear = 1905 
+};
