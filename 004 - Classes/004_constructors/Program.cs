8ef1532ff45cb02187b1daff146c7d6276e6f108
lukaofirst <lucasmacedo123@gmail.com>
2023-02-13 07:15:40 -0300

    Classes - constructors

---
+++ 004 - Classes/004_constructors/Program.cs
@@ -0,0 +1,6 @@
+﻿using _004_constructors.Entities;
+
+/* --- Constructors --- */
+
+var user1 = new User(1, "Lorem", 19, "lorem@gmail.com");
+var user2 = new User(2, "Ipsum", 22, "ipsum@gmail.com");
