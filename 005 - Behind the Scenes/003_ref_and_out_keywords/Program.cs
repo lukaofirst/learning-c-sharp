e3743aa2c8c8c806e2a3ecd9ae797496c38e54d3
lukaofirst <lucasmacedo123@gmail.com>
2023-02-21 16:06:34 -0300

    Behind the Scenes - ref and out keywords

---
+++ 005 - Behind the Scenes/003_ref_and_out_keywords/Program.cs
@@ -0,0 +1,36 @@
+﻿/* --- ref and out keywords --- */
+
+/* - ref - */
+// you must initialize the 'x' variable
+int x = 10;
+Calculator.TripleNumberUsingRef(ref x);
+Console.WriteLine(x);
+
+/* - out - */
+// you don't need to initialize the variable
+int y = 20;
+int z;
+Calculator.TripleNumberUsingOut(y, out z);
+Console.WriteLine(z);
+
+// Example: using .TryParse() to validate a string as a valid number
+var customerDocument = "11111111111";
+
+if(long.TryParse(customerDocument, out long result))
+{
+    Console.WriteLine("It's a valid document");
+}
+
+
+class Calculator
+{
+    public static void TripleNumberUsingRef(ref int x)
+    {
+        x *= 3;
+    }
+
+    public static void TripleNumberUsingOut(int a, out int result)
+    {
+        result = a * 3;
+    }
+}
