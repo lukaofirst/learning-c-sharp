a602a4ff1da6e608c57c45cbf4076dbbeccc2507
lukaofirst <lucasmacedo123@gmail.com>
2023-02-21 15:02:15 -0300

    Behind the Scenes - access modifiers

---
+++ 005 - Behind the Scenes/001_access_modifiers/Program.cs
@@ -0,0 +1,19 @@
+﻿/* --- Access Modifiers --- */
+
+// private (most restrict)
+// Access is limited to within the class definition
+
+// internal
+// Access is limited exclusively to classes defined within the current project assembly
+
+// protected
+// Access is limited to within the class definition and any derived class
+
+// private protected
+// It's the combination between private and protected keywords
+
+// protected internal
+// It's the combination of protected and internal keywords
+
+// public (most accessible)
+// There are no restrictions on accessing public members
