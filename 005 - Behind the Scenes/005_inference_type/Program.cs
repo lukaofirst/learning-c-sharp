63d2fd9e6ca700770c93d03d839ffe7ea742fa30
lukaofirst <lucasmacedo123@gmail.com>
2023-02-21 16:52:21 -0300

    Behind the Scenes - inference type

---
+++ 005 - Behind the Scenes/005_inference_type/Program.cs
@@ -0,0 +1,12 @@
+﻿/* --- Inference Type --- */
+
+// The compiler tries to infer the type using the 'var' keyword
+// once assigned, you cannot reassign to another type
+
+// It's considered a good practice to let the compiler infer the type
+
+var exampleStr = "hello world";
+var exampleNum = 10;
+
+Console.WriteLine(exampleStr);
+Console.WriteLine(exampleNum);
