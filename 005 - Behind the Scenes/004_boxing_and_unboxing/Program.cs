f7e09d227079debda5eba43ae7437f5fab75a2cf
lukaofirst <lucasmacedo123@gmail.com>
2023-02-21 16:42:24 -0300

    Behind the Scenes - boxing and unboxing

---
+++ 005 - Behind the Scenes/004_boxing_and_unboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿/* --- Boxing and Unboxing --- */
+
+/* - Boxing - */
+// implicit => the compiler will try to convert int to a object type
+// object is a super type in C#
+int x = 10;
+object obj = x;
+Console.WriteLine(obj);
+
+/* - Unboxing - */
+// explicit => you need to tell the compiler the correct type
+// do not use the double type here, otherwise you'll receive
+// a runtime error (InvalidCastException)
+// unless if you use the Convert.ToDouble() method
+int y = (int)obj;
+
+Console.WriteLine(y);
