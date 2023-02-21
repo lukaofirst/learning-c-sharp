47ed91d0b048d2fd5a96e53ab46269240dd4f8df
lukaofirst <lucasmacedo123@gmail.com>
2023-02-21 15:02:31 -0300

    Behind the Scenes - params keyword

---
+++ 005 - Behind the Scenes/002_params_keyword/Program.cs
@@ -0,0 +1,23 @@
+﻿/* --- params keyword --- */
+
+var numbersArr = new int[] { 1, 2, 3 };
+
+var result1 = Calculator.Sum(numbersArr);
+var result2 = Calculator.Sum(20, 10, 50);
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+
+class Calculator
+{
+    public static int Sum(params int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+}
