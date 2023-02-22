b4016328312a3faf9b860e61cb7f748d9f7310ea
lukaofirst <lucasmacedo123@gmail.com>
2023-02-22 13:12:15 -0300

    Behind the Scenes - string buiders

---
+++ 005 - Behind the Scenes/009_string_builders/Program.cs
@@ -0,0 +1,25 @@
+﻿/* --- String Builders --- */
+
+using System.Text;
+
+var str = "Hello world from C#";
+
+// Faster
+var strBuilder = new StringBuilder();
+
+foreach (var item in str)
+{
+    strBuilder.Append(item);
+}
+
+Console.WriteLine(strBuilder.ToString());
+
+// Slower
+//var str2 = string.Empty;
+
+//foreach(var item in str)
+//{
+//    str2 += str;
+//}
+
+//Console.WriteLine(str2);
