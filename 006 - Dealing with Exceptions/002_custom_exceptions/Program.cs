eaf13f16278e65b481da2346ebd715bed9b0ae88
lukaofirst <lucasmacedo123@gmail.com>
2023-02-23 12:17:03 -0300

    Dealing with Exceptions - custom exceptions

---
+++ 006 - Dealing with Exceptions/002_custom_exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿/* --- Custom Exceptions --- */
+
+// Custom exception should derive from Exception class
+// instead of ApplicationException
+
+var age = 16;
+var adultAge = 21;
+
+if (age < adultAge)
+	throw new CustomException("Invalid Age! To be considered adult, age must be greater than 21");
+
+public class CustomException : Exception
+{
+	public CustomException(string message) : base(message)
+	{
+
+	}
+}
