eca19e37206c91cb8c4094bdbc6c324918df956a
lukaofirst <lucasmacedo123@gmail.com>
2023-02-23 12:16:09 -0300

    Dealing with Exceptions - built-in exceptions

---
+++ 006 - Dealing with Exceptions/001_built_in_exceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿/* --- Built-in Exceptions --- */
+
+// There're more built-in exceptions, but some common exceptions are:
+
+// ArgumentNullException
+// An argument passed to a method is null
+
+// ArgumentOutOfRangeException
+// Thrown when the value of an argument is outside the range of the values
+
+// DividedByZeroException
+// The denominator in an integer or decimal is zero
+
+// IndexOutOfRangeException
+// An index is outside the bounds of an array or collection
+
+// NullReferenceException
+// Thrown when a program access members of null object 
+
+// OutOfMemoryException
+// Thrown when a program does not get enought memory to execute the code
+
+// StackOverflowException
+// Thrown when a stack in memory overflows
+
+
