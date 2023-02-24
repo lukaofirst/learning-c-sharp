caaf216d8009169dfc04e2e1fb03b7900aa8b29f
lukaofirst <lucasmacedo123@gmail.com>
2023-02-24 09:49:57 -0300

    Dealing with Exceptions - try-catch statement

---
+++ 006 - Dealing with Exceptions/003_try_catch_statement/Program.cs
@@ -0,0 +1,20 @@
+﻿/* --- try-catch Statement --- */
+
+// You can filter for multiple exceptions, adding multiples catches
+// to handle them in different ways
+
+// Remember to always add the "general" catch Exception at the end
+
+try
+{
+	// code that has the best scenario
+}
+catch (Exception)
+{
+	// handle exception here
+	throw;
+}
+finally
+{
+	// cleanup the unnused resources
+}
