ab0dcbb83cb5f4740db298449d193e9cc127a780
lukaofirst <lucasmacedo123@gmail.com>
2023-02-27 11:22:07 -0300

    Delegates - creating a delegate

---
+++ 007 - Delegates/001_delegate/Program.cs
@@ -0,0 +1,19 @@
+﻿/* --- Delegate --- */
+
+// Ways to set a target method
+CustomDelegate del1 = new CustomDelegate(PrintMessage);
+CustomDelegate del2 = PrintMessage;
+CustomDelegate del3 = (string msg) => Console.WriteLine(msg);
+
+// Ways to execute a delegate
+del1.Invoke("Hello world");
+del2("Just a message to be printed");
+del3("Another message");
+
+
+static void PrintMessage(string message)
+{
+    Console.WriteLine($"Called by PrintMessage() method with parameter: {message}");
+}
+
+delegate void CustomDelegate(string message);
