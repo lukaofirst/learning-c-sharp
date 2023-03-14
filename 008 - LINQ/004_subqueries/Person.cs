2d08fed40feac0b7495ce9ed88be37fc5d0ac689
lukaofirst <lucasmacedo123@gmail.com>
2023-03-14 09:32:43 -0300

    LINQ - subqueries

---
+++ 008 - LINQ/004_subqueries/Person.cs
@@ -0,0 +1,8 @@
+namespace _004_subqueries
+{
+	public class Person
+	{
+		public string Name { get; set; } = string.Empty;
+		public int Age { get; set; }
+	}
+}
---
+++ 008 - LINQ/004_subqueries/Program.cs
@@ -0,0 +1,24 @@
+﻿/* --- Subqueries --- */
+
+using _004_subqueries;
+
+var personsList = new List<Person>
+{
+	new Person { Name = "Bill", Age = 25 },
+	new Person { Name = "Will", Age = 65 },
+	new Person { Name = "Jane", Age = 70 },
+	new Person { Name = "Jonathan", Age = 16 },
+	new Person { Name = "Walter", Age = 37 },
+	new Person { Name = "Mathews", Age = 29 },
+};
+
+var filteredByPersonNames = new List<string>
+{
+	"Bill", "Jane", "Will"
+};
+
+var query = personsList
+	.Where(x => filteredByPersonNames.Contains(x.Name));
+
+foreach (var item in query)
+	Console.WriteLine(item.Name);
