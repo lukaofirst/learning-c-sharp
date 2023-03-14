96b7129b14ace04a5ce8f702e1a11932aec52173
lukaofirst <lucasmacedo123@gmail.com>
2023-03-14 09:32:02 -0300

    LINQ - linq method syntax

---
+++ 008 - LINQ/001_linq_method_syntax/Program.cs
@@ -0,0 +1,15 @@
+﻿/* --- LINQ Method Syntax --- */
+var list = new List<string>()
+{
+	"C#",
+	"JavaScript",
+	"TypeScript",
+	"Dart",
+	"React",
+	"Angular"
+};
+
+var query = list.Where(x => x.Contains("a"));
+
+foreach (var item in query)
+	Console.WriteLine(item);
