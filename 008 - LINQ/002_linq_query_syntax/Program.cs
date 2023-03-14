59466e85a05131764f9e448fb337990b9cf955c8
lukaofirst <lucasmacedo123@gmail.com>
2023-03-14 09:32:13 -0300

    LINQ - linq query syntax

---
+++ 008 - LINQ/002_linq_query_syntax/Program.cs
@@ -0,0 +1,20 @@
+﻿/* --- LINQ Query Syntax --- */
+
+var list = new List<string>()
+{
+	"C#",
+	"JavaScript",
+	"TypeScript",
+	"Dart",
+	"React",
+	"Angular"
+};
+
+var query = (
+	from item in list
+	where item.Contains("a")
+	select item
+);
+
+foreach (var item in query)
+	Console.WriteLine(item);
