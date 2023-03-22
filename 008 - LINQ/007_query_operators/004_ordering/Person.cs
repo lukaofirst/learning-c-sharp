baa224726b0106edde835e7a39453d34ed70d6f2
lukaofirst <lucasmacedo123@gmail.com>
2023-03-21 21:28:19 -0300

    LINQ - query operators - ordering

---
+++ 008 - LINQ/007_query_operators/004_ordering/Person.cs
@@ -0,0 +1,14 @@
+namespace _004_ordering
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+
+		public Person(string name, int age)
+		{
+			Name = name;
+			Age = age;
+		}
+	}
+}
---
+++ 008 - LINQ/007_query_operators/004_ordering/Program.cs
@@ -0,0 +1,22 @@
+﻿/* --- Query Operators - Ordering --- */
+
+using _004_ordering;
+
+var personList = new List<Person>()
+{
+	new Person("Oliver", 22),
+	new Person("Oliver", 16),
+	new Person("John", 30),
+	new Person("Bill", 40),
+	new Person("Harry", 28),
+};
+
+/* --- .OrderBy() and .ThenBy() --- */
+var orderAndThenByResult = personList.OrderBy(x => x.Name);
+orderAndThenByResult.ToList().ForEach(x => Console.WriteLine($"{x.Name} | {x.Age}"));
+Console.WriteLine();
+
+/* --- .OrderByDescending() and .ThenByDescending() --- */
+var orderAndThenByDescendingResult = personList.OrderByDescending(x => x.Name).ThenByDescending(x => x.Age);
+orderAndThenByDescendingResult.ToList().ForEach(x => Console.WriteLine($"{x.Name} | {x.Age}"));
+Console.WriteLine();
