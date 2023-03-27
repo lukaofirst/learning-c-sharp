a8eb26a6b708484ddff197deeb87f79c08ddce2e
lukaofirst <lucasmacedo123@gmail.com>
2023-03-27 09:47:41 -0300

    Files and Streams - FIleStream and StreamReader

---
+++ 010 - Files and Streams/002_filestream_and_streamreader/Program.cs
@@ -0,0 +1,49 @@
+﻿/* --- FileStream and StreamReader --- */
+
+var sourcePath = @"./files/source.txt";
+
+//FileStream? fs = null;
+
+// more verbose
+// try
+// {
+// 	fs = new FileStream(sourcePath, FileMode.Open);
+// 	var sr1 = new StreamReader(fs);
+
+// 	while (!sr1.EndOfStream)
+// 	{
+// 		var line = sr1.ReadLine();
+// 		Console.WriteLine(line);
+// 	}
+// }
+// catch (IOException ex)
+// {
+// 	Console.WriteLine(ex.Message);
+// }
+// finally
+// {
+// 	if (fs != null)
+// 		fs.Close();
+// }
+
+// // less verbose
+StreamReader? sr = null;
+try
+{
+	sr = File.OpenText(sourcePath);
+
+	while (!sr.EndOfStream)
+	{
+		var line = sr.ReadLine();
+		Console.WriteLine(line);
+	}
+}
+catch (IOException ex)
+{
+	Console.WriteLine(ex.Message);
+}
+finally
+{
+	if (sr != null)
+		sr.Close();
+}
