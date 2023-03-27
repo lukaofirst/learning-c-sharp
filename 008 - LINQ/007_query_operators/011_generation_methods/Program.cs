3c585a47037b441acd8f05d85626c03b2728c020
lukaofirst <lucasmacedo123@gmail.com>
2023-03-27 08:42:57 -0300

    LINQ - query operators - generation methods

---
+++ 008 - LINQ/007_query_operators/011_generation_methods/Program.cs
@@ -0,0 +1,22 @@
+﻿/* --- Query Operators - Generation Methods --- */
+
+/* - Range - */
+var rangeResult = Enumerable.Range(10, 5);
+rangeResult.ToList().ForEach(x => Console.WriteLine(x));
+Console.WriteLine();
+
+/* - Repeat - */
+var repeatResult = Enumerable.Repeat(5, 5);
+repeatResult.ToList().ForEach(x => Console.WriteLine(x));
+Console.WriteLine();
+
+// Generating random CPFs
+var random = new Random();
+var chars = "0123456789";
+const int cpfLength = 11;
+
+var result = Enumerable.Repeat(chars, cpfLength)
+	.Select(x => x[random.Next(x.Length)])
+	.ToArray();
+
+Console.WriteLine(result);
