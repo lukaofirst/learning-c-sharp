0de9da9f54ab0695a7e8c0b2e28996b21266faf8
lukaofirst <lucasmacedo123@gmail.com>
2023-03-27 11:41:44 -0300

    Files and Streams - StreamWriter

---
+++ 010 - Files and Streams/004_streamwriter/Program.cs
@@ -0,0 +1,18 @@
+﻿/* --- StreamWriter --- */
+
+// using dotnet CLI it works fine
+var targetPath = "./files/target.txt";
+
+// but in case if you're not
+// you have to "find" the root directory of your project
+// var absPath = Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.FullName;
+// var fullPath = absPath + targetPath;
+//Console.WriteLine(absPath);
+
+var sw = new StreamWriter(targetPath);
+
+sw.WriteLine("lorem ipsum dolor sit amet");
+sw.WriteLine("consectetur adipiscing elit");
+sw.WriteLine("suspendisse aliquet eget nisi non ultricies");
+
+sw.Close();
