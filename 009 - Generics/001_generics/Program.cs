c5687963376e7ffe13eb1f2a0c22cb9b12b94985
lukaofirst <lucasmacedo123@gmail.com>
2023-03-27 08:53:41 -0300

    Generics

---
+++ 009 - Generics/001_generics/Program.cs
@@ -0,0 +1,20 @@
+﻿/* --- Generics --- */
+
+using System.Collections;
+
+// Without Generics
+// They are more costly, because they have to perform boxing and unboxing
+var arrayList = new ArrayList();
+
+arrayList.Add(10);
+arrayList.Add("lorem");
+arrayList.Add(true);
+
+// With Generics
+// It performs better because the compiler already know which type 
+// we are working with, not allowing adding "any" type
+var list = new List<string>();
+
+list.Add("lorem");
+list.Add("10");
+list.Add("true");
