8bbdc0c78adfca5420596081facce8e4863e5d77
lukaofirst <lucasmacedo123@gmail.com>
2023-03-27 13:11:06 -0300

    Files and Streams - Directory and DirectoryInfo

---
+++ 010 - Files and Streams/005_directory_and_directoryinfo/Program.cs
@@ -0,0 +1,19 @@
+﻿/* --- Directory and DirectoryInfo --- */
+
+var currentDirectory = Directory.GetCurrentDirectory();
+Console.WriteLine(currentDirectory);
+
+var directoryExists = new DirectoryInfo(currentDirectory).Exists;
+Console.WriteLine(directoryExists);
+
+var directories = Directory.EnumerateDirectories(currentDirectory, "*.*", SearchOption.AllDirectories);
+foreach (var item in directories)
+	Console.WriteLine(item);
+
+Console.WriteLine();
+
+var files = Directory.EnumerateFiles(currentDirectory, "*.*", SearchOption.AllDirectories);
+foreach (var item in files)
+	Console.WriteLine(item);
+
+
