3fde20ce2cf334cc6b5ce95b7a55fbf602000e70
lukaofirst <lucasmacedo123@gmail.com>
2023-03-27 09:31:20 -0300

    Files and Streams - File and FileInfo

---
+++ 010 - Files and Streams/001_file_and_fileinfo/Program.cs
@@ -0,0 +1,17 @@
+﻿/* --- File and FileInfo --- */
+
+try
+{
+	var sourcePath = @"./files/source.txt";
+	var targetPath = @"./files/target.txt";
+
+	var content = File.ReadAllLines(sourcePath);
+	File.Copy(sourcePath, targetPath);
+
+	foreach (var line in content)
+		Console.WriteLine(line);
+}
+catch (IOException ex)
+{
+	Console.WriteLine(ex.Message);
+}
