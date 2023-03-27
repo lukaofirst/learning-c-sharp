0380b291ba10a41dbf569668d1dbe4596a3c9f95
lukaofirst <lucasmacedo123@gmail.com>
2023-03-27 09:54:09 -0300

    Files and Streams - using block

---
+++ 010 - Files and Streams/003_using_block/Program.cs
@@ -0,0 +1,28 @@
+﻿/* --- using block --- */
+
+var sourcePath = @"./files/source.txt";
+
+try
+{
+	// using var fs = new FileStream(sourcePath, FileMode.Open);
+	// using var sr1 = new StreamReader(fs);
+
+	// while (!sr1.EndOfStream)
+	// {
+	// 	var line = sr1.ReadLine();
+	// 	Console.WriteLine(line);
+	// }
+
+	using var sr = File.OpenText(sourcePath);
+
+	while (!sr.EndOfStream)
+	{
+		var line = sr.ReadLine();
+		Console.WriteLine(line);
+	}
+
+}
+catch (IOException ex)
+{
+	Console.WriteLine(ex.Message);
+}
