179279f79beec23b2a0ad1fd05336779f102ff62
lukaofirst <lucasmacedo123@gmail.com>
2023-03-27 20:00:29 -0300

    Files and Streams - Path

---
+++ 010 - Files and Streams/006_path/Program.cs
@@ -0,0 +1,12 @@
+﻿/* --- Path --- */
+
+var sourcePath = "learning-c-sharp/010 - Files and Streams/006_path/files/source.txt";
+
+Console.WriteLine($"DirectorySeparatorChar: {Path.DirectorySeparatorChar}");
+Console.WriteLine($"PathSeparator: {Path.PathSeparator}");
+Console.WriteLine($"GetDirectoryName: {Path.GetDirectoryName(sourcePath)}");
+Console.WriteLine($"GetFileName: {Path.GetFileName(sourcePath)}");
+Console.WriteLine($"GetExtension: {Path.GetExtension(sourcePath)}");
+Console.WriteLine($"GetFileNameWithoutExtension: {Path.GetFileNameWithoutExtension(sourcePath)}");
+Console.WriteLine($"GetFullPath: {Path.GetFullPath(sourcePath)}");
+Console.WriteLine($"GetTempPath: {Path.GetTempPath()}");
